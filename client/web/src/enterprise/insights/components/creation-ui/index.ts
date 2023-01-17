0d30defbb31b20d067e83edefd320fac6d4fe9a9
vovakulikov <vovakulikov@icloud.com>
2023-01-17 20:07:00 -0300

    Update creation ui validators and types (from string to array of strings)

--- client/web/src/enterprise/insights/components/creation-ui/index.ts
+++ client/web/src/enterprise/insights/components/creation-ui/index.ts
@@ -1,7 +1,7 @@
 export * from './live-preview'
 export * from './creation-ui-layout/CreationUiLayout'
 
-export { getSanitizedRepositories, getSanitizedSeries } from './sanitizers'
+export { getSanitizedSeries } from './sanitizers'
 
 export { CodeInsightDashboardsVisibility } from './CodeInsightDashboardsVisibility'
 export { CodeInsightTimeStepPicker } from './code-insight-time-step-picker/CodeInsightTimeStepPicker'
@@ -12,7 +12,6 @@ export type { EditableDataSeries } from './form-series'
 export {
     insightTitleValidator,
     insightRepositoriesValidator,
-    insightRepositoriesAsyncValidator,
     insightStepValueValidator,
     insightSeriesValidator,
 } from './validators/validators'
--- client/web/src/enterprise/insights/components/creation-ui/sanitizers/index.ts
+++ client/web/src/enterprise/insights/components/creation-ui/sanitizers/index.ts
@@ -1,2 +1,1 @@
-export { getSanitizedRepositories } from './repositories'
 export { getSanitizedSeries } from './series'
